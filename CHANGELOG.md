7e47e799ae109c49d54e9286ac5046d6dcacab7a
João Magalhães <joamag@gmail.com>
2021-06-28 06:00:51 +0100

    version: 0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+
+* Initial working support for email notifications (Notification API) - [#4](https://github.com/ripe-tech/ripe-id/issues/4)
+
+### Changed
+
+*
+
+### Fixed
+
+*
+
+## [0.1.0] - 2021-06-25
+
+### Added
+
+* Initial working version
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+setuptools.setup(
+    name = "mingify",
+    version = "0.1.0",
+    author = "Hive Solutions Lda.",
+    author_email = "development@hive.pt",
+    description = "Mingify",
+    license = "Apache License, Version 2.0",
+    keywords = "mingify url minification",
+    url = "http://www.hive.pt",
+    zip_safe = False,
+    packages = [
+        "mingify",
+        "mingify.controllers",
+        "mingify.controllers.api",
+        "mingify.controllers.web",
+        "mingify.models"
+    ],
+    package_dir = {
+        "" : os.path.normpath("src")
+    },
+    install_requires = [
+        "appier",
+        "appier_extras",
+        "jinja2",
+        "pymongo"
+    ],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
